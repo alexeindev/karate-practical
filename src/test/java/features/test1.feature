7b3064cd9b9d0898f6b56b55726b07ca7dd4105e
Alejandra Lopez <alexeindev@gmail.com>
2022-11-23 15:26:44 -0500

    First commit

---
+++ src/test/java/features/test1.feature
@@ -0,0 +1,4 @@
+Feature: Print hello
+
+  Scenario: hello
+    * print 'hello world'
---
+++ src/test/java/testRunner.java
@@ -0,0 +1,9 @@
+import com.intuit.karate.junit5.Karate;
+
+public class testRunner {
+    @Karate.Test
+    Karate testAll() {
+        return Karate.run().relativeTo(getClass());
+    }
+
+}
